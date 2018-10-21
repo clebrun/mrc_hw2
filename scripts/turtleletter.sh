30cacd5253659432f98f3dfaf3f8f98c913e8113
Caliph Lebrun <clebrun@nps.edu>
2018-10-21 16:59:54 -0700

    Adding turtleletter.sh to the repository

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,5 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, -4.5]'
+
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 0.5]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[9.0, 0.0, 0.0]' '[0.0, 0.0, 5.0]'
